821e8c806b4b9612d41845916e2b510b62c5af6b
Thamiresvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2026-06-12 20:36:28 -0300

    fazendo tarefa

--- praticasAula05/pratica05.js
+++ praticasAula05/pratica05.js
@@ -6,6 +6,25 @@ console.log(5 > 20 && 5 < 2); // false
 console.log(5 === 5 || 5 === "5" );
 
 // not ! é falso
-console.log(!20 > 50);
+console.log(!(20 > 50) );
+
 
 // && =  é, || = ou, ! = não e
+
+
+// verificar se 10 é maior que cinco ou 10 é menor que 20
+console.log("-".repeat(50))
+console.log("Atividade Prática")
+console.log(10 > 5 && 10 < 20); 
+
+// verificar se 10-5 é menor que 10 ou 10=5 é igual a 15
+console.log(15 < 10 || 15 === 15);
+
+// verificar se 30 não é igual e 40
+console.log(!(30 === 40));
+
+// verificar se 8 não é igual a 3 e 12 e menor que 5
+console.log(!(8 > 3) && 12 < 5);
+
+// verificar se 25 é maior que 20 ou 10 é maior que 50
+console.log(25 > 20 || 10 > 50);
