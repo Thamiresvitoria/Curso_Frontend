db6489ff40509115df64d0241736ce9035977d98
Thamiresvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2026-06-12 20:22:43 -0300

    Adicionando Curso_Frontend

--- praticasAula05/pratica05.js
+++ praticasAula05/pratica05.js
@@ -1,6 +1,11 @@
 // Usando if e else
 
 console.log(5 > 20 && 5 < 2); // false
-  
-    // && =  é, || = ou, ! = não e
-let nome 
+
+// our || é ou
+console.log(5 === 5 || 5 === "5" );
+
+// not ! é falso
+console.log(!20 > 50);
+
+// && =  é, || = ou, ! = não e
