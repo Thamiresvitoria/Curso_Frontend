104fbd744e4dc600defe9e325e3039cba3a929e2
Thamiresvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2026-06-25 19:31:44 -0300

    aa

---
+++ praticasAula10/vidas.js
@@ -0,0 +1,13 @@
+// Vidas de um jogador em um jogo.
+
+let vidas = 3;
+
+while(vidas > 0){
+  console.log("jogando..");
+  vidas--;
+}
+
+console.log("Gamer over!");
+
+// Assistir episódio de uma series
+
