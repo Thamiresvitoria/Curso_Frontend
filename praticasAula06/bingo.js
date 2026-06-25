2dec2db41c0c239d82678bf669e449b53168310e
Thamiresvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2026-06-25 19:31:23 -0300

    aa

---
+++ praticasAula06/bingo.js
@@ -0,0 +1,15 @@
+const TOTAL_NUMEROS = 75;
+let numeroDisponivel = [];
+let numerosSorteados = [];
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+if (numerosDisponivel.length ==- 0){
+  console.log("\nTodos os números jpa foram sorteados!\n");
+  return;
+}
